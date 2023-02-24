98ce540e021aa79c71e84838a33b975fb656436e
phiarchitect <phi@phiarchitect.com>
2023-02-24 13:50:51 -0800

    fix parents

--- geometor/model/model.py
+++ geometor/model/model.py
@@ -7,6 +7,8 @@ Model class
 
 from .common import *
 
+from rich import print
+
 class Model(list):
 
     """Docstring for Model. """
@@ -14,24 +16,26 @@ class Model(list):
     def __init__(self):
         """TODO: to be defined. """
         super().__init__(self)
-        self.parents = defaultdict(set)
+        self.parents = defaultdict(dict)
         self.classes = defaultdict(list)
+        self.labels = defaultdict(str)
 
 
-    def set_point(self, x_val, y_val, parents=set(), classes=[]) -> spg.Point:
+    def set_point(self, x_val, y_val, parents={}, classes=[], label='') -> spg.Point:
         '''generate a sympy.geometry.Point'''
         print(f'set_point:')
         print(f'    {x_val=}')
         print(f'    {y_val=}')
         print(f'    {parents=}')
         print(f'    {classes=}')
+        print(f'    {label=}')
         x_val = sp.simplify(x_val)
         y_val = sp.simplify(y_val)
         pt = spg.Point(x_val, y_val)
-        return self.add_point(pt, parents, classes)
+        return self.add_point(pt, parents, classes, label)
 
 
-    def add_point(self, pt: spg.Point, parents=set(), classes=[]) -> spg.Point:
+    def add_point(self, pt: spg.Point, parents={}, classes=[], label='') -> spg.Point:
         '''add point to model
         find duplicates
         clean values
@@ -44,28 +48,35 @@ class Model(list):
             pt = spg.Point(x, y)
             if pt in self.points():
                 # add attributes
-                self.parents[pt].update(parents)
+                for parent in parents:
+                    self.parents[pt][parent] = ''
                 self.classes[pt].extend(classes)
+                #  self.labels[pt] = label
                 return pt
 
             else:
                 for prev_pt in self.points():
                     if pt.equals(prev_pt):
-                        self.parents[prev_pt].update(parents)
+                        for parent in parents:
+                            self.parents[prev_pt][parent] = ''
                         self.classes[prev_pt].extend(classes)
+                        #  self.labels[prev_pt] = label
                         return prev_pt
 
             #  logging.info(f'  + {pt}')
             self.append(pt)
-            self.parents[pt].update(parents)
+            for parent in parents:
+                self.parents[pt][parent] = ''
             self.classes[pt].extend(classes)
+            self.labels[pt] = label
+
             print(f'    add_point: {pt}')
             return pt
         else:
             logging.info('    not a point')
 
 
-    def construct_line(self, pt1: spg.Point, pt2: spg.Point, classes=[]) -> spg.Line:
+    def construct_line(self, pt_1: spg.Point, pt_2: spg.Point, classes=[], label='') -> spg.Line:
         """
         create `spg.Line` object
         add `add_line`
@@ -74,11 +85,12 @@ class Model(list):
         print(f'    {pt_1=}')
         print(f'    {pt_2=}')
         print(f'    {classes=}')
-        struct = spg.Line(pt1, pt2)
-        return self.add_line(struct, classes)
+        print(f'    {label=}')
+        struct = spg.Line(pt_1, pt_2)
+        return self.add_line(struct, classes, label)
 
 
-    def add_line(self, struct: spg.Line, classes=[]) -> spg.Line:
+    def add_line(self, struct: spg.Line, classes=[], label='') -> spg.Line:
         '''
         Add ``line`` to list.
         check for duplicates in elements.
@@ -90,7 +102,8 @@ class Model(list):
             # check by reference
             if struct in self.lines():
                 print_log('struct exists')
-                self.parents[struct].update(struct.points)
+                for parent in struct.points:
+                    self.parents[struct][parent] = ''
                 self.classes[struct].extend(classes)
                 return struct
             else:
@@ -105,30 +118,33 @@ class Model(list):
                         {el}
                         {prev}
                         ''')
-                        self.parents[prev].update(struct.points)
+                        for parent in struct.points:
+                            self.parents[prev][parent] = ''
                         self.classes[prev].extend(classes)
                         return prev
 
                 # add struct
                 self.append(struct)
                 print(f'    add_struct: {struct}')
-                self.parents[struct].update(struct.points)
+                for parent in struct.points:
+                    self.parents[struct][parent] = ''
                 self.classes[struct].extend(classes)
+                self.labels[struct] = label
 
                 # check intersections
                 for prev in self.structs():
                     if not struct.equals(prev):
                         results = struct.intersection(prev)
                         for pt in results:
-                            self.add_point(pt, parents={prev, struct})
-                            self.parents[prev].update({pt})
-                            self.parents[struct].update({pt})
+                            self.add_point(pt, parents={prev:'', struct:''})
+                            self.parents[prev][pt] = ''
+                            self.parents[struct][pt] = ''
                 return struct
         else:
             print_log('not a line')
 
 
-    def construct_circle(self, center_pt: spg.Point, radius_pt: spg.Point, classes=[]) -> spg.Circle:
+    def construct_circle(self, center_pt: spg.Point, radius_pt: spg.Point, classes=[], label='') -> spg.Circle:
         """
         create line object from points
         add_circle
@@ -139,12 +155,13 @@ class Model(list):
         print(f'    {classes=}')
         radius_len = center_pt.distance(radius_pt)
         print(f'    {radius_len=}')
+        print(f'    {label=}')
         struct = spg.Circle(center_pt, radius_len)
         struct.radius_pt = radius_pt
-        return self.add_circle(struct, classes)
+        return self.add_circle(struct, classes, label)
 
 
-    def add_circle(self, struct: spg.Circle, classes={}) -> spg.Circle:
+    def add_circle(self, struct: spg.Circle, classes=[], label='') -> spg.Circle:
         """
         add circle to model
         """
@@ -153,7 +170,8 @@ class Model(list):
             # check by reference
             if struct in self.circles():
                 print_log('struct exists')
-                self.parents[struct].update({struct.center, struct.radius_pt})
+                self.parents[struct][struct.center] = ''
+                self.parents[struct][struct.radius_pt] = ''
                 self.classes[struct].extend(classes)
                 return struct
             else:
@@ -163,35 +181,43 @@ class Model(list):
                     diff = (prev.equation().simplify() - struct.equation().simplify()).simplify()
                     #  logging.info(f'    > diff: {diff}')
                     if not diff:
-                        self.parents[prev].update({struct.center, struct.radius_pt})
+                        self.parents[prev][struct.center] = ''
+                        self.parents[prev][struct.radius_pt] = ''
                         self.classes[prev].extend(classes)
                         return prev
 
                 # add struct
                 self.append(struct)
                 print(f'    add_struct: {struct}')
-                self.parents[struct].update({struct.center, struct.radius_pt})
+                self.parents[struct][struct.center] = ''
+                self.parents[struct][struct.radius_pt] = ''
                 self.classes[struct].extend(classes)
+                self.labels[struct] = label
+
                 # check intersections
                 for prev in self.structs():
                     if not struct.equals(prev):
                         results = struct.intersection(prev)
                         for pt in results:
-                            self.add_point(pt, parents={prev, struct})
-                            self.parents[prev].update({pt})
-                            self.parents[struct].update({pt})
+                            self.add_point(pt, parents={prev:'', struct:''})
+                            self.parents[prev][pt] = ''
+                            self.parents[struct][pt] = ''
                 return struct
         else:
             print_log('not a circle')
 
 
-    def set_polygon(self, poly_pts, classes=[], style={}):
+    def set_polygon(self, poly_pts, classes=[], label=''):
         '''- takes array of points - make sympy.geometry.Polygon, Triangle or Segment'''
         el = spg.Polygon(*poly_pts)
-        return self.add_polygon(el, classes=classes)
+        print(f'set_polygon: ')
+        print(f'    {poly_pts=}')
+        print(f'    {classes=}')
+        print(f'    {label=}')
+        return self.add_polygon(el, classes, label)
 
 
-    def add_polygon(self, poly: spg.Polygon, classes=[]) -> spg.Polygon:
+    def add_polygon(self, poly: spg.Polygon, classes=[], label='') -> spg.Polygon:
         '''
         Add ``line`` to list.
         check for duplicates in elements.
@@ -200,18 +226,25 @@ class Model(list):
         '''
         # add struct
         self.append(poly)
-        self.parents[poly].update(poly.vertices)
+        for parent in poly.vertices:
+            self.parents[poly][parent] = ''
         self.classes[poly].extend(classes)
+        self.labels[poly] = label
 
         return poly
 
-    def set_segment(self, pt_1, pt_2, classes=[], style={}):
+    def set_segment(self, pt_1, pt_2, classes=[], label=''):
         '''- takes 2 points - make sympy.geometry.Segment'''
         el = spg.Segment(pt_1, pt_2)
-        return self.add_segment(el, classes=classes)
+        print(f'set_segment: ')
+        print(f'    {pt_1=}')
+        print(f'    {pt_2=}')
+        print(f'    {classes=}')
+        print(f'    {label=}')
+        return self.add_segment(el, classes, label)
 
 
-    def add_segment(self, seg: spg.Segment, classes=[]) -> spg.Segment:
+    def add_segment(self, seg: spg.Segment, classes=[], label='') -> spg.Segment:
         '''
         Add ``line`` to list.
         check for duplicates in elements.
@@ -220,8 +253,10 @@ class Model(list):
         '''
         # add struct
         self.append(seg)
-        self.parents[seg].update(seg.vertices)
+        for parent in seg.points:
+            self.parents[seg][parent] = ''
         self.classes[seg].extend(classes)
+        self.labels[seg] = label
 
         return seg
 
@@ -239,7 +274,13 @@ class Model(list):
         filtered list of structs
         currently lines and circles
         """
-        return [el for el in self if isinstance(el, spg.Line) or isinstance(el, spg.Circle)]
+        structs = []
+        temp = [el for el in self if isinstance(el, spg.Line) or isinstance(el, spg.Circle)]
+        for el in temp:
+            if 'guide' not in self.classes[el]:
+                structs.append(el)
+
+        return structs
 
 
     def lines(self):
